2572bb8cde11e49d75fc01310c5dfbd884bc8229
Alvin-Karanja <alvin2@protonmail.com>
2019-10-29 21:57:42 +0300

    Add Orders method

--- src/main/java/model/Orders.java
+++ src/main/java/model/Orders.java
@@ -4,4 +4,11 @@ public class Orders {
     private String login;
     private int idProduct;
     private int numberOfProducts;
+
+    public Orders(String login, int idProduct, int numberOfProducts) {
+        super();
+        this.login = login;
+        this.idProduct = idProduct;
+        this.numberOfProducts = numberOfProducts;
+    }
 }
