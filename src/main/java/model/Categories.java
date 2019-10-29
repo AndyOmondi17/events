acde7d49493f3e556609d0e4b8bd4a4d711d94e4
Alvin-Karanja <alvin2@protonmail.com>
2019-10-29 21:42:11 +0300

    Add categories method

--- src/main/java/model/Categories.java
+++ src/main/java/model/Categories.java
@@ -4,4 +4,11 @@ public class Categories {
     private String categoryName;
     private String description;
     private String imagePath;
+
+    public Categories(String categoryName, String description, String imagePath) {
+        super();
+        this.categoryName = categoryName;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
 }
