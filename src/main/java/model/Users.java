68c530eaa952dc1327ab389b529868f25d85c61f
Alvin-Karanja <alvin2@protonmail.com>
2019-10-30 07:20:00 +0300

    Add access modifiers

---
+++ src/main/java/model/Users.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Users {
+
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
+    private int age;
+    private String city;
+    private double money;
+}
