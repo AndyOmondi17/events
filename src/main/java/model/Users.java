bc7389f3785c2f8679742ac097a5779254940e3f
Alvin-Karanja <alvin2@protonmail.com>
2019-10-30 07:20:40 +0300

    Add method users

--- src/main/java/model/Users.java
+++ src/main/java/model/Users.java
@@ -9,4 +9,15 @@ public class Users {
     private int age;
     private String city;
     private double money;
+
+    public Users(String login, String password, String name, String surname, int age, String city, double money) {
+        super();
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.city = city;
+        this.money = money;
+    }
 }
